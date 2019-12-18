5cfc235fcf99741633ce204bccd2d11e4f7fd658
smack1n <mackintosh.scott@gmail.com>
2019-12-18 15:59:25 -0700

    challenge2 commit

---
+++ challenge2/challenge2.py
@@ -0,0 +1,31 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+class challenge2(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("../chromedriver")
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_challenge2(self):
+        self.driver.get("https://www.copart.com/")
+        self.assertIn("Auto Auction - Copart USA - Salvage Cars for Sale in Online Car Auctions", self.driver.title)
+
+        self.driver.find_element(By.ID, "input-search").click()
+        self.driver.find_element(By.ID, "input-search").send_keys("exotics")
+
+        self.driver.find_element_by_xpath("//div[2]/button").click()
+        self.driver.find_element_by_xpath("//div[2]/button").send_keys(Keys.RETURN)
+
+        assert "No result" not in self.driver.page_source
+        self.driver.implicitly_wait(10)
+        result = self.driver.find_element_by_xpath("// *[text() = 'PORSCHE']").text
+        assert (result == 'PORSCHE'), "PORSCHE not found."
+        print("PORSCHE listed.")
+
+if __name__ == '__main__':
+    unittest.main()
