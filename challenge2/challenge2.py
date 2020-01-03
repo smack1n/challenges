7b986b85cedbdab8c97029763b821abf19df56e4
smack1n <mackintosh.scott@gmail.com>
2020-01-03 15:50:45 -0700

    test commit

--- challenge2/challenge2.py
+++ challenge2/challenge2.py
@@ -22,9 +22,12 @@ class challenge2(unittest.TestCase):
         self.driver.find_element_by_xpath("//div[2]/button").send_keys(Keys.RETURN)
 
         assert "No result" not in self.driver.page_source
+
         self.driver.implicitly_wait(10)
         result = self.driver.find_element_by_xpath("// *[text() = 'PORSCHE']").text
+
         assert (result == 'PORSCHE'), "PORSCHE not found."
+
         print("PORSCHE listed.")
 
 if __name__ == '__main__':
---
+++ rokuHelp/rokuHelp.py
@@ -0,0 +1,37 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+class slingChannels(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("../chromedriver")
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_slingChannels(self):
+        self.driver.get("https://help.sling.com/")
+        self.assertIn("Help Center", self.driver.title)
+
+        self.driver.find_element(By.ID, "support-search-input").click()
+
+        self.driver.find_element(By.ID, "support-search-input").send_keys("roku")
+
+        self.driver.find_element_by_xpath("//div[2]/button").send_keys(Keys.RETURN)
+
+        self.driver.implicitly_wait(10)
+
+        elements = self.driver.find_elements_by_xpath("/html/body/div[3]/div/div[2]/div/div[1]/div[2]/ul/section")
+
+        number = len(elements)
+        print(number)
+
+        for item in elements:
+
+            print(item.text)
+
+if __name__ == '__main__':
+    unittest.main()
