b29779bf198e3d74cb94542028ac5b70f6eda9f4
smack1n <mackintosh.scott@gmail.com>
2020-06-04 19:23:18 -0600

    test commit

---
+++ challenge5/challenge5.py
@@ -0,0 +1,65 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from common.TopNavSearch import TopNavSearch
+import time
+
+class challenge5(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome('../chromedriver')
+
+    def test_checkForModel(self):
+        self.driver.get("https://www.copart.com/")
+        self.assertIn("Auto Auction - Copart USA - Salvage Cars for Sale in Online Car Auctions", self.driver.title)
+        self.driver.implicitly_wait(5)
+
+        query = "Porsche"
+        TopNavSearch(self.driver).runSearch(query)
+
+        self.driver.find_element(By.XPATH, "//select[@name='serverSideDataTable_length']").click()
+        self.driver.implicitly_wait(5)
+        self.driver.find_element(By.XPATH, "//option[@value='100']").click()
+        self.driver.implicitly_wait(10)
+        time.sleep(3)
+
+        all_models = self.driver.find_elements(By.XPATH, "//table[@id='serverSideDataTable']//span[@data-uname='lotsearchLotmodel']")
+
+        print("\n" + "Total unique models:" + "\n")
+
+        print(len(all_models))
+
+        models = []
+        for model in all_models:
+
+            models.append(model.text) #adds model to models list
+
+        #print(models)
+
+        uniquemodelset = set(models) #removes duplicates using set
+
+        print(len(uniquemodelset)) #check removal of duplicates
+
+        uniquemodeldict = {str(list(uniquemodelset)[0]): 0}
+
+        #print(uniquemodeldict)
+
+        for unique_model in uniquemodelset:
+            uniquemodeldict.update({unique_model: 0})
+
+        #print(uniquemodeldict)
+
+        for unique_model in uniquemodelset:
+            for model in models:
+                if model == unique_model:
+                    uniquemodeldict[unique_model] += 1
+
+            print("There are " + str(uniquemodeldict[unique_model]) + " " + unique_model + " on the page.")
+
+    def tearDown(self):
+        self.driver.close()
+        print("Tear down")
+
+if __name__ == '__main__':
+    unittest.main()
